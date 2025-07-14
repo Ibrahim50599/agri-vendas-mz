13eee80ef87507b39b021479e83e4ddd1555c206
Ibrahim Hagi Cassamo <brahimhagicassamo@gmail.com>
2025-07-14 10:45:45 +0200

    Primeiro commit do projeto

--- main.py
+++ main.py
@@ -930,9 +930,7 @@ def contato_whatsapp(vendedor_id):
     flash('Vendedor não encontrado')
     return redirect(url_for('index'))
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
 
 if __name__ == '__main__':
     init_db()
