3b35ae96300714169d321cbfc18b581ce9f3e436
Ibrahim Hagi Cassamo <brahimhagicassamo@gmail.com>
2025-07-14 11:00:27 +0200

    Primeiro commit do projeto

--- start.py
+++ start.py
@@ -1,9 +1,8 @@
 from main import init_db, app
+import os
 
-# Inicializa o banco de dados com as tabelas necessárias
-init_db()
-
-# Inicia o servidor Flask no ambiente do Render
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    init_db()
+    os.system("gunicorn main:app")
+
 
